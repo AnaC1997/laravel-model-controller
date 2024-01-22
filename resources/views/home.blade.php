fb2223e91e2977065837e85470211b35955f5593
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2024-01-22 18:04:34 +0100

    style css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,19 @@
 
    
 
-<h1>Movies</h1>
+<h1 class="text-center">Movies</h1>
    
-<div class="d-flex">
-    <div class="card" style="width: 18rem;">
-        @foreach ($movies as $movie)
+<div class="d-flex justify-content-between">
+    @foreach ($movies as $movie)
+    <div class="card m-4" style="width: 18rem;">
         <img src="{{ asset( $movie['imagine']) }}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5>{{$movie->nome}}</h5>
           <p class="card-text">{{$movie->descrizione}}</p>
         </div>
-        @endforeach
+       
     </div>
+    @endforeach
 </div>
 
 
